0702786650dd4a19198b3f6c1bce828127370b9b
popuu <popuu@DESKTOP-J5T1MRH>
2018-07-05 03:57:37 +0900

    되는거

--- sdsfoodbot/main.py
+++ sdsfoodbot/main.py
@@ -1,20 +1,10 @@
-# Copyright 2016 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# -*- coding: utf-8 -*-
 
-# [START app]
 import logging
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from datetime import datetime
+import urllib
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 
@@ -22,10 +12,51 @@ app = Flask(__name__)
 def hello():
     return 'SDS FOOD BOT!'
 
+@app.route('/keyboard')
+def Keyboard():
+    dataSend = {
+        "type": "buttons",
+        "buttons": ["잠실식단"]
+    }
+    return jsonify(dataSend)
+
+@app.route('/message', methods=['POST'])
+def Message():
+    dataReceive = request.get_json()
+    content = dataReceive['content']
+    if content == u"잠실식단":
+        data = urllib.urlopen('http://www.sdsfoodmenu.co.kr:9106/foodcourt/menuplanner/list').read()
+        bs = BeautifulSoup(data, 'html.parser')
+        g = bs.find_all("span", {'style' : 'font-size: 16px;font-weight: bold'})
+        a = ""
+        for i, menu in enumerate(g):
+            a += str(i+1) +". "+ menu.get_text() + "\n"
+            
+        a += "\n맛있는 식사하세요!"
+        dataSend = {
+            "message": {
+                "text": a
+            },
+            "keyboard": {
+                "type": "buttons",
+                "buttons": ["잠실식단"]
+            }
+        }
+    elif content == u"도움말":
+        dataSend = {
+            "message": {
+               "text": "도움말인데 클났음"
+            },
+            "keyboard": {
+                "type": "buttons",
+                "buttons": ["잠실식단"]
+            }
+        }
+    return jsonify(dataSend)
+
 
 @app.errorhandler(500)
 def server_error(e):
     # Log the error and stacktrace.
     logging.exception('An error occurred during a request.')
     return 'An internal error occurred.', 500
-# [END app]
