8e6ef29e9cd4269621a7194fc4e02cc843238522
popuu <popuu@DESKTOP-J5T1MRH>
2018-07-27 01:54:47 +0900

    db저장용 List추가 랜덤추천에 image 추가

--- sdsfoodbot/main.py
+++ sdsfoodbot/main.py
@@ -2,6 +2,7 @@
 
 import logging
 import random
+import time
 from flask import Flask, request, jsonify
 from datetime import datetime
 from sdsfood import sdsfood
@@ -23,16 +24,17 @@ def Keyboard():
 @app.route('/message', methods=['POST'])
 def Message():
     dataReceive = request.get_json()
+    start = time.time()
     content = dataReceive['content']
-    if content == u"잠실식단":
-        bs_sdsfood = sdsfood()
-        str_message = ""
-        
-        check = bs_sdsfood.get_open()
-        if check :
-        
-            str_message += bs_sdsfood.get_day()
-            
+    bs_sdsfood = sdsfood()
+    str_message = ""
+    img_url = ""
+    check = bs_sdsfood.get_open()
+    menu_db_list = []
+    if check :
+        menu_db_list = bs_sdsfood.get_menu_db_list()
+        str_message += bs_sdsfood.get_day()
+        if content == u"잠실식단":
             b1_kor1 = bs_sdsfood.get_menu(u"[코1] ", "E59C-group-item")
             b1_kor2 = bs_sdsfood.get_menu(u"[코2] ", "E59D-group-item")
             b1_west = bs_sdsfood.get_menu(u"[웨스] ", "E59E-group-item") 
@@ -54,29 +56,11 @@ def Message():
             str_message += strB1
             if b2_xing :
                 str_message += strB2
-                
             str_message += u"\n맛있는 식사하세요!"
-        else :
-            str_message = u"즐거운 휴일되세요!"
-        dataSend = {
-            "message": {
-                "text": str_message
-            },
-            "keyboard": {
-                "type": "buttons",
-                "buttons": ["잠실식단", "랜덤추천"]
-            }
-        }
-    elif content == u"랜덤추천":
-        bs_sdsfood = sdsfood()
-        str_message = ""
-        
-        check = bs_sdsfood.get_open()
-        if check :
-        
-            str_message += bs_sdsfood.get_day()
+            img_url = random.choice(menu_db_list)[4]
+        elif content == u"랜덤추천":
+            """
             menu_list = []
-            
             b1_kor1 = bs_sdsfood.get_menu_list(u"\n* B1\n[코1] ", "E59C-group-item")
             b1_kor2 = bs_sdsfood.get_menu_list(u"\n* B1\n[코2] ", "E59D-group-item")
             b1_west = bs_sdsfood.get_menu_list(u"\n* B1\n[웨스] ", "E59E-group-item") 
@@ -91,20 +75,38 @@ def Message():
             b2_asia = bs_sdsfood.get_menu_list(u"\n* B2\n[아시] ", "E5EA-group-item")
             b2_chef = bs_sdsfood.get_menu_list(u"\n* B2\n[쉐프] ", "E5EB-group-item")
     
-            menu_list = b1_kor1 + b1_kor2 + b1_west + b1_tang + b1_gats + b1_take + b2_xing + b2_myun + b2_poli + b2_bibi + b2_asia + b2_chef 
-            str_message += random.choice(menu_list)
+            menu_list = b1_kor1 + b1_kor2 + b1_west + b1_tang + b1_gats + b1_take + b2_xing + b2_myun + b2_poli + b2_bibi + b2_asia + b2_chef
+            """
+            menu = []
+            menu = random.choice(menu_db_list)
+            str_message += u"\n* "+menu[6]+"\n"+menu[5]+" "+menu[1]
+            str_message += "\n"
+            str_message += menu[3]
             str_message += u"\n\n추천메뉴와 함께 맛있는 식사하세요!"
-        else :
-            str_message = u"즐거운 휴일되세요!"
-        dataSend = {
-            "message": {
-               "text": str_message
-            },
-            "keyboard": {
-                "type": "buttons",
-                "buttons": ["잠실식단", "랜덤추천"]
-            }
+            img_url = menu[4]
+    else :
+        str_message = u"즐거운 휴일되세요!"       
+    
+    end = time.time() - start
+    end = str(end)
+    dataSend = {
+        "message": {
+           "text": str_message+"\n"+end,
+            "photo": {
+                "url": img_url,
+                "width": 640,
+                "height": 480
+              }
+        },
+        "message_button": {
+            "label": "잠실메뉴",
+            "url": "http://www.sdsfoodmenu.co.kr:9106/foodcourt/menuplanner/list"
+        },
+        "keyboard": {
+            "type": "buttons",
+            "buttons": ["잠실식단", "랜덤추천"]
         }
+    }   
     return jsonify(dataSend)
 
 
--- sdsfoodbot/sdsfood.py
+++ sdsfoodbot/sdsfood.py
@@ -1,7 +1,17 @@
+# -*- coding: utf-8 -*-
+
 import urllib
 from bs4 import BeautifulSoup
 
 class sdsfood:
+    #코1, 코2, 웨스, 탕맛, 가즈, 테킷, 씽푸, 미각, 나폴, 비빈, 아시, 쉐프
+    store_id_list = ("E59C", "E59D", "E59E", "E59F", "E59G", "E59H", "E5E6", "E5E7", "E5E8", "E5E9", "E5EA", "E5EB")
+    store_name_list = {"E59C":u"[코1]", "E59D":u"[코2]", "E59E":u"[웨스]", "E59F":u"[탕맛]", "E59G":u"[가츠]", "E59H":u"[테킷]"
+                       , "E5E6":u"[씽푸]", "E5E7":u"[미각]", "E5E8":u"[나폴]", "E5E9":u"[비빈]", "E5EA":u"[아시]", "E5EB":u"[쉐프]"}
+    store_floor_list = {"E59C":u"B1", "E59D":u"B1", "E59E":u"B1", "E59F":u"B1", "E59G":u"B1", "E59H":u"B1"
+                   , "E5E6":u"B2", "E5E7":u"B2", "E5E8":u"B2", "E5E9":u"B2", "E5EA":u"B2", "E5EB":u"B2"}
+
+   
     def __init__(self):
         url = 'http://www.sdsfoodmenu.co.kr:9106/foodcourt/menuplanner/list'
         data = urllib.urlopen(url).read()
@@ -45,3 +55,41 @@ class sdsfood:
             for i in store_list:
                 menu.append(store_str+" "+i.get_text())
         return menu
+    
+    def get_menu_db_list(self):
+        menu_list = []
+        for id in self.store_id_list:
+            store = self.bs.find("div", class_=id+"-group-item")
+            menu = []
+            if store :
+                menu.append(id)
+                store_list = store.find_all('tr')
+                for i in store_list:
+                    #print(i)
+                    menu_name = i.find("span", {'style' : 'font-size: 16px;font-weight: bold'})
+                    menu_material = i.find("span", {'style' : 'font-size: 10px;color: #737373'})
+                    menu_kcal = i.find("span", {'style' : 'display: inline-block;font-size: 10px;color: #adadad;margin-top: 3px'})
+                    menu_img = i.img['src']
+                    
+                    if menu_name:
+                        menu.append(menu_name.get_text())
+                    else : 
+                        menu.append("")
+                    if menu_material:
+                        menu.append(menu_material.get_text())
+                    else :
+                        menu.append("")
+                    if menu_kcal:
+                        menu.append(menu_kcal.get_text())
+                    else :
+                        menu.append("")
+                    if menu_img:
+                        menu.append(menu_img)
+                    else:
+                        menu.append("")
+                    menu.append(self.store_name_list[id])
+                    menu.append(self.store_floor_list[id])
+                    
+            if menu :              
+                menu_list.append(menu)
+        return menu_list
