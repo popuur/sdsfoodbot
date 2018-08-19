a9c81d4bd8734201752fd66c6198acf4c23bd68c
popuu <popuu@DESKTOP-J5T1MRH>
2018-08-20 03:57:47 +0900

    - cronJob(주기적 크롤링) - gcp ndb 저장/사용

--- sdsfoodbot/main.py
+++ sdsfoodbot/main.py
@@ -21,6 +21,12 @@ def Keyboard():
     }
     return jsonify(dataSend)
 
+@app.route('/get_menu')
+def GetMenu():
+    bs_sdsfood = sdsfood()
+    day_list = bs_sdsfood.get_day_list_from_db()
+    return jsonify(day_list)
+
 @app.route('/message', methods=['POST'])
 def Message():
     dataReceive = request.get_json()
@@ -31,61 +37,39 @@ def Message():
     img_url = ""
     check = bs_sdsfood.get_open()
     menu_db_list = []
+       
     if check :
-        menu_db_list = bs_sdsfood.get_menu_db_list()
-        str_message += bs_sdsfood.get_day()
+        menu_db_list = bs_sdsfood.get_day_list_from_db()
+        str_message += menu_db_list[1]
         if content == u"잠실식단":
-            b1_kor1 = bs_sdsfood.get_menu(u"[코1] ", "E59C-group-item")
-            b1_kor2 = bs_sdsfood.get_menu(u"[코2] ", "E59D-group-item")
-            b1_west = bs_sdsfood.get_menu(u"[웨스] ", "E59E-group-item") 
-            b1_tang = bs_sdsfood.get_menu(u"[탕맛] ", "E59F-group-item")
-            b1_gats = bs_sdsfood.get_menu(u"[가츠] ", "E59G-group-item")
-            b1_take = bs_sdsfood.get_menu(u"[테킷] ", "E59H-group-item")
-          
-            b2_xing = bs_sdsfood.get_menu(u"[씽푸] ", "E5E6-group-item")
-            b2_myun = bs_sdsfood.get_menu(u"[미각] ", "E5E7-group-item")
-            b2_poli = bs_sdsfood.get_menu(u"[나폴] ", "E5E8-group-item")
-            b2_bibi = bs_sdsfood.get_menu(u"[비빈] ", "E5E9-group-item")
-            b2_asia = bs_sdsfood.get_menu(u"[아시] ", "E5EA-group-item")
-            b2_chef = bs_sdsfood.get_menu(u"[쉐프] ", "E5EB-group-item")
+            strB1 = u"\n* B1\n" 
+            strB2 = u"\n* B2\n" 
             
-            
-            strB1 = u"\n* B1\n" + b1_kor1 + b1_kor2 + b1_west + b1_tang + b1_gats + b1_take
-            strB2 = u"\n* B2\n" + b2_xing + b2_myun + b2_poli + b2_bibi + b2_asia + b2_chef 
+            cntB2 = 0
+            for menu in menu_db_list[2]:
+                if menu[6] == u"B1":
+                    strB1 += menu[5]+" "+menu[1]+"\n"
+                elif menu[6] == u"B2":
+                    strB2 += menu[5]+" "+menu[1]+"\n"
+                    cntB2 += 1
             
             str_message += strB1
-            if b2_xing :
+            if cntB2 > 0:
                 str_message += strB2
+            
             str_message += u"\n맛있는 식사하세요!"
-            img_url = random.choice(menu_db_list)[4]
+            img_url = random.choice(menu_db_list[2])[4]
         elif content == u"랜덤추천":
-            """
-            menu_list = []
-            b1_kor1 = bs_sdsfood.get_menu_list(u"\n* B1\n[코1] ", "E59C-group-item")
-            b1_kor2 = bs_sdsfood.get_menu_list(u"\n* B1\n[코2] ", "E59D-group-item")
-            b1_west = bs_sdsfood.get_menu_list(u"\n* B1\n[웨스] ", "E59E-group-item") 
-            b1_tang = bs_sdsfood.get_menu_list(u"\n* B1\n[탕맛] ", "E59F-group-item")
-            b1_gats = bs_sdsfood.get_menu_list(u"\n* B1\n[가츠] ", "E59G-group-item")
-            b1_take = bs_sdsfood.get_menu_list(u"\n* B1\n[테킷] ", "E59H-group-item")
-          
-            b2_xing = bs_sdsfood.get_menu_list(u"\n* B2\n[씽푸] ", "E5E6-group-item")
-            b2_myun = bs_sdsfood.get_menu_list(u"\n* B2\n[미각] ", "E5E7-group-item")
-            b2_poli = bs_sdsfood.get_menu_list(u"\n* B2\n[나폴] ", "E5E8-group-item")
-            b2_bibi = bs_sdsfood.get_menu_list(u"\n* B2\n[비빈] ", "E5E9-group-item")
-            b2_asia = bs_sdsfood.get_menu_list(u"\n* B2\n[아시] ", "E5EA-group-item")
-            b2_chef = bs_sdsfood.get_menu_list(u"\n* B2\n[쉐프] ", "E5EB-group-item")
-    
-            menu_list = b1_kor1 + b1_kor2 + b1_west + b1_tang + b1_gats + b1_take + b2_xing + b2_myun + b2_poli + b2_bibi + b2_asia + b2_chef
-            """
+           
             menu = []
-            menu = random.choice(menu_db_list)
+            menu = random.choice(menu_db_list[2])
             str_message += u"\n* "+menu[6]+"\n"+menu[5]+" "+menu[1]
             str_message += "\n"
             str_message += menu[3]
             str_message += u"\n\n추천메뉴와 함께 맛있는 식사하세요!"
             img_url = menu[4]
     else :
-        str_message = u"즐거운 휴일되세요!"       
+        str_message = u"즐거운 휴일되세요!"    
     
     end = time.time() - start
     end = str(end)
--- sdsfoodbot/sdsfood.py
+++ sdsfoodbot/sdsfood.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
 
 import urllib
+import datetime
 from bs4 import BeautifulSoup
+from google.appengine.ext import ndb
+ 
+class sds_menu(ndb.Model):
+    day_hour = ndb.StringProperty()
+    food_day = ndb.StringProperty()
+    food_menu = ndb.JsonProperty()
+
 
 class sdsfood:
     #코1, 코2, 웨스, 탕맛, 가즈, 테킷, 씽푸, 미각, 나폴, 비빈, 아시, 쉐프
@@ -16,11 +24,13 @@ class sdsfood:
         url = 'http://www.sdsfoodmenu.co.kr:9106/foodcourt/menuplanner/list'
         data = urllib.urlopen(url).read()
         self.bs = BeautifulSoup(data, 'html.parser')
-        
+
+    #날짜 가져오기        
     def get_day (self):
         day = self.bs.find("span", {'style' : 'font-size: 13px; color: #d6a066'}).get_text().split()
         return " ".join(day) + "\n"
     
+    #오픈일 확인
     def get_open (self):
         result = False
         open = self.bs.find(class_="notice-bold")
@@ -30,7 +40,7 @@ class sdsfood:
             result = True
         return result
 
-
+    #class id로 찾아 파싱
     def get_menu(self, store_str, classId):
         store = self.bs.find("div", class_=classId)
         if store :
@@ -47,6 +57,7 @@ class sdsfood:
             store = ""
         return store
     
+    #class id로 찾아서 메뉴 파싱
     def get_menu_list(self, store_str, classId):
         store = self.bs.find("div", class_=classId)
         menu = []
@@ -56,42 +67,110 @@ class sdsfood:
                 menu.append(store_str+" "+i.get_text())
         return menu
     
-    def get_menu_db_list(self):
+    def get_menu_from_parse(self, classId, menu_html):
+        #0:id, 1:name, 2:material, 3:kcal, 4:img, 5:store_name, 6:floor
+        menu = [] 
+        menu.append(classId)
+        menu_name = menu_html.find("span", {'style' : 'font-size: 16px;font-weight: bold'})
+        menu_material = menu_html.find("span", {'style' : 'font-size: 10px;color: #737373'})
+        menu_kcal = menu_html.find("span", {'style' : 'display: inline-block;font-size: 10px;color: #adadad;margin-top: 3px'})
+        menu_img = menu_html.find("img")
+        
+        if menu_name:
+            menu.append(menu_name.get_text())
+        else : 
+            menu.append("")
+        if menu_material:
+            menu.append(menu_material.get_text())
+        else :
+            menu.append("")
+        if menu_kcal:
+            menu.append(menu_kcal.get_text())
+        else :
+            menu.append("")
+        if menu_img:
+            menu_img = menu_html.img['src']
+            menu.append(menu_img)
+        else:
+            menu.append("")
+        menu.append(self.store_name_list[classId])
+        menu.append(self.store_floor_list[classId])
+        
+        return menu
+        
+    
+    #class id로 받아오고 파싱추가
+    def get_menu_list_from_parse(self):
         menu_list = []
         for id in self.store_id_list:
             store = self.bs.find("div", class_=id+"-group-item")
-            menu = []
+            menu = [] #0:id, 1:name, 2:material, 3:kcal, 4:img, 5:store_name, 6:floor
             if store :
-                menu.append(id)
                 store_list = store.find_all('tr')
                 for i in store_list:
-                    #print(i)
-                    menu_name = i.find("span", {'style' : 'font-size: 16px;font-weight: bold'})
-                    menu_material = i.find("span", {'style' : 'font-size: 10px;color: #737373'})
-                    menu_kcal = i.find("span", {'style' : 'display: inline-block;font-size: 10px;color: #adadad;margin-top: 3px'})
-                    #menu_img = i.img['src']
-                    menu_img = i.find("img")
-                    
-                    if menu_name:
-                        menu.append(menu_name.get_text())
-                    else : 
-                        menu.append("")
-                    if menu_material:
-                        menu.append(menu_material.get_text())
-                    else :
-                        menu.append("")
-                    if menu_kcal:
-                        menu.append(menu_kcal.get_text())
-                    else :
-                        menu.append("")
-                    if menu_img:
-                        menu_img = i.img['src']
-                        menu.append(menu_img)
-                    else:
-                        menu.append("")
-                    menu.append(self.store_name_list[id])
-                    menu.append(self.store_floor_list[id])
-                    
-            if menu :              
-                menu_list.append(menu)
+                    menu = self.get_menu_from_parse(id, i)                   
+                    if menu :              
+                        menu_list.append(menu)
+                        
         return menu_list
+    
+    def put_daily_menu_list_to_db(self, day_list):
+        day_db = sds_menu()        
+        day_db.day_hour = day_list[0]
+        day_db.food_day = day_list[1]
+        day_db.food_menu = day_list[2]
+        day_db.key = ndb.Key('sds_menu', day_list[0])
+        day_db.put()
+        
+
+    def check_time(self, hour):
+        order = 0
+        
+        if hour >= 1 and hour < 8 :
+            order = 2
+        elif hour >= 8 and hour < 20 :
+            order = 3
+        else :
+            order = 1
+        return order
+
+    def get_order_time(self):
+        dt = datetime.datetime.now()
+        day_hour = dt.strftime("%H")
+        day_order = self.check_time(day_hour)
+        day_num = dt.strftime("%Y%m%d%H")+"_"+str(day_order)
+        return day_num
+    
+    
+    def get_day_list_from_parse(self):
+        menu_list = self.get_menu_list_from_parse()
+        store_date = self.get_day()
+        day_num = self.get_order_time()
+        
+        day_list = []
+        day_list.append(day_num)
+        day_list.append(store_date)                        
+        day_list.append(menu_list)
+        
+        return day_list
+    
+    def put_day_list_to_db(self):
+        day_list = self.get_day_list_from_parse()
+        self.put_daily_menu_list_to_db(day_list)
+        return day_list
+    
+    def get_day_list_from_db(self):
+        day_num = self.get_order_time()
+        day_key = ndb.Key('sds_menu', day_num)
+        day_menu = []
+        day_get = day_key.get()
+        
+        if day_get :
+            day_menu.append(day_get.day_hour)
+            day_menu.append(day_get.food_day)
+            day_menu.append(day_get.food_menu)
+           
+        else :
+            day_menu = self.put_day_list_to_db()
+
+        return day_menu
